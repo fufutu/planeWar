4b367304fc9865de535acb2ef222ff3245d6a20c
李军 <mark_lijun@sina.com>
2014-09-26 19:31:11 +0800

    Update myplane.js

--- js/myplane.js
+++ js/myplane.js
@@ -1,6 +1,4 @@
-/**
- * Created by Administrator on 2014/8/20.
- */
+
 var fps=55;
 var boxx=0;
 var boxy=0;
@@ -74,38 +72,37 @@ function drawBullet() {
         }
     }
 }
-    function produceBullet() {//定义一个Hero类,//x 表示坦克的 横坐标, y 表示纵坐标, direct 方向
+    function produceBullet() {
         heroBullet = new Bullet(planex + planewidth / 2, planey + 10);
-        heroBullets.push(heroBullet);//把这个子弹放在数组中
-        var timer = window.setInterval("heroBullets[" + (heroBullets.length - 1) + "].run()", 50);//就算你工作过一两年，你也不一定能想到这儿,把子弹数组所有子弹同时启动,每个子弹定时器是独立的，按原来的方法heroBullet.run(),50,所有子弹共享一个定时器，子弹速度会越来越快
-        heroBullets[(heroBullets.length - 1)].timer = timer;//把这个Timer传递给子弹，js对象是引用传递
+        heroBullets.push(heroBullet);
+        var timer = window.setInterval("heroBullets[" + (heroBullets.length - 1) + "].run()", 50);
+        heroBullets[(heroBullets.length - 1)].timer = timer;
 
     }
 
-    function procuceEnemy() {//定义一个Hero类,//x 表示坦克的 横坐标, y 表示纵坐标, direct 方向
+    function procuceEnemy() {
         var x = Math.ceil(Math.random() * (boxwidth - planewidth));
         theEnemy = new Enemy(x, 33);
-        allEnemy.push(theEnemy);//把这个子弹放在数组中
-        var timer = window.setInterval("allEnemy[" + (allEnemy.length - 1) + "].run()", 50);//就算你工作过一两年，你也不一定能想到这儿,把子弹数组所有子弹同时启动,每个子弹定时器是独立的，按原来的方法heroBullet.run(),50,所有子弹共享一个定时器，子弹速度会越来越快
-        allEnemy[(allEnemy.length - 1)].timer = timer;//把这个Timer传递给子弹，js对象是引用传递
+        allEnemy.push(theEnemy);
+        var timer = window.setInterval("allEnemy[" + (allEnemy.length - 1) + "].run()", 50);
+        allEnemy[(allEnemy.length - 1)].timer = timer;
     }
 
-    function Enemy(x, y) {//-------------改进处9
+    function Enemy(x, y) {
         this.x = x;
         this.y = y;
         this.timer = null;
         this.isLive = true;
         this.run = function run() {
 
-            //在该表这个子弹的坐标时，我们先判断子弹是否已经到边界
-            //子弹不前进，有两个逻辑，1.碰到边界，2. 碰到敌人坦克.好像没检查碰到敌人坦克这种情况在isHitEnemy函数中检查--------改进处9
+           
             if (this.y > boxheight || this.isLive == false) {
 
-                window.clearInterval(this.timer);//子弹要停止.
+                window.clearInterval(this.timer);
 
                 this.isLive = false;
             }
-            //子弹死亡
+
             else {
                 this.y += 2.5;
 
